2522c3e4030055c8c44fd5b6f62c9b9909f7501f
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-08-14 16:01:48 +0700

    fix(pages): Change class names

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import RegisterForm from '../modules/RegisterForm/components/RegisterForm';
-import styles from './register.module.scss'
+import styles from './styles/register.module.scss'
 
 function register() {
   return (
-    <section className={styles.auth}>
-      <div className={styles['auth__content']}>
+    <section className={styles.register}>
+      <div className={styles['register__content']}>
         <RegisterForm />
       </div>
     </section>
